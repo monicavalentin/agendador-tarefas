a58610b0047a0c0a6f38a368f0169a5b64a68539
Monicat263 <mda263@bol.com.br>
2026-02-05 12:13:13 -0300

    Commit inicial

---
+++ src/test/java/com/mvalentin/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvalentin.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
